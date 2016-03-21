b4eaf93ee18c30c432c5e4f5ba4742ccba57b6e3
ymbelyakova@gmail.com <ymbelyakova@gmail.com>
2016-03-21 16:43:03 +0300

    Initial Import

---
+++ src/main/java/selenium_java_training/pages/Page.java
@@ -0,0 +1,29 @@
+package selenium_java_training.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Abstract class representation of a Page in the UI. Page object pattern
+ */
+public abstract class Page {
+
+	protected WebDriver webDriver;
+
+	/*
+	 * Constructor injecting the WebDriver interface
+	 * 
+	 * @param webDriver
+	 */
+	public Page(WebDriver webDriver) {
+		this.webDriver = webDriver;
+	}
+
+	public WebDriver getWebDriver() {
+		return webDriver;
+	}
+
+	public String getTitle() {
+		return webDriver.getTitle();
+	}
+
+}
